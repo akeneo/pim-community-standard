d73c1fccd310da10efacd40b3a7452d50cba04c3
Nicolas Dupont <nicolas@akeneo.com>
2013-09-24 19:29:31 +0200

    add install sh

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+ENV="prod"
+if [ $1 ]
+then
+    ENV="$1"
+fi
+
+php app/console doctrine:database:create--env $env
+php app/console oro:entity-extend:clear --env $ENV
+php app/console doctrine:schema:create --env $ENV
+php app/console doctrine:fixture:load --no-debug --no-interaction --env $ENV
+php app/console oro:navigation:init --env $ENV
+php app/console oro:entity-config:init --env $ENV
+php app/console oro:entity-extend:init --env $ENV
+php app/console oro:entity-extend:update-config --env $ENV
+php app/console doctrine:schema:update --env $ENV --force
+php app/console oro:search:create-index --env $ENV
+php app/console oro:navigation:init --env $ENV
+php app/console assets:install web --env $ENV
+php app/console assetic:dump --env $ENV
+php app/console oro:assetic:dump --env $ENV
+php app/console oro:translation:dump --env $ENV
+php app/console pim:search:reindex en_US --env $env
+php app/console pim:versioning:refresh --env $env
+php app/console pim:product:completeness-calculator --env $env
+
