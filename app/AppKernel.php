4c8d83973654634174a118a697e6b7e2ccfa8a20
Nicolas Dupont <nicolas@akeneo.com>
2015-08-17 19:17:27 +0200

    PIM-4445: prepare the 1.4.0-beta2

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,7 +19,7 @@ class AppKernel extends Kernel
     {
         $bundles = [
             // your app bundles should be registered here
-            // new Acme\Bundle\AppBundle\AcmeAppBundle(),
+            //new Acme\Bundle\AppBundle\AcmeAppBundle(),
         ];
 
         if (in_array($this->getEnvironment(), array('dev', 'test', 'behat'))) {
@@ -104,9 +104,11 @@ class AppKernel extends Kernel
             new Knp\Bundle\GaufretteBundle\KnpGaufretteBundle(),
             new APY\JsFormValidationBundle\APYJsFormValidationBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new Oneup\FlysystemBundle\OneupFlysystemBundle(),
             new Akeneo\Bundle\MeasureBundle\AkeneoMeasureBundle(),
             new Akeneo\Bundle\BatchBundle\AkeneoBatchBundle(),
             new Akeneo\Bundle\StorageUtilsBundle\AkeneoStorageUtilsBundle(),
+            new Akeneo\Bundle\FileStorageBundle\AkeneoFileStorageBundle(),
         ];
     }
 
