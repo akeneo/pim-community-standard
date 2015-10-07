5e749edcc7bdb0e307b704a56eec5e94a7e45010
Jean-Marie Leroux <jmleroux.pro@gmail.com>
2015-10-07 15:47:57 +0200

    PIM-4928 : media migration 1.3 - 1.4

--- upgrades/1.3-1.4/orm/migrate_medias.php
+++ upgrades/1.3-1.4/orm/migrate_medias.php
@@ -1,5 +1,6 @@
 <?php
 
+use Pim\Upgrade\ORM\MediaMigration;
 use Symfony\Component\Console\Input\ArgvInput;
 use Symfony\Component\Console\Output\ConsoleOutput;
 
@@ -7,7 +8,8 @@ require_once __DIR__ . '/../../../app/bootstrap.php.cache';
 require_once __DIR__ . '/../../../app/AppKernel.php';
 require_once __DIR__ . '/../../SchemaHelper.php';
 require_once __DIR__ . '/../../UpgradeHelper.php';
-require_once __DIR__ . '/../common/MediaMigration.php';
+require_once __DIR__ . '/../common/AbstractMediaMigration.php';
+require_once __DIR__ . '/MediaMigration.php';
 
 // TO BE LAUNCHED AFTER HAVING UPDATED THE DEPENDENCIES OF YOUR PROJECT
 // NOT ABLE TO USE DOCTRINE ORM/MONGODB AS DEPENDENCIES HAVE BEEN UPDATED BUT NOT THE DATABASE AT THIS POINT
@@ -24,13 +26,17 @@ require_once __DIR__ . '/../common/MediaMigration.php';
 
 $migration = new MediaMigration(new ConsoleOutput(), new ArgvInput($argv));
 
-$mediaTable = $migration->getProductMediaTable();
-$valueTable = $migration->getSchemaHelper()->getTableOrCollection('product_value');
+$mediaTable           = $migration->getProductMediaTable();
+$valueTable           = $migration->getSchemaHelper()->getTableOrCollection('product_value');
+$productTemplateTable = $migration->getSchemaHelper()->getTableOrCollection('product_template');
 
 $migration->createFileInfoTable();
 $migration->storeLocalMedias();
-$migration->setOriginalFilenameToMedias($valueTable, $mediaTable);
+
+$migration->setOriginalFilenameToMedias($mediaTable);
 $migration->migrateMediasOnProductValue($valueTable, $mediaTable, 'FK_93A1BBF3EA9FDD75');
+$migration->migrateMediasOnProductTemplate($productTemplateTable, $mediaTable);
+
 $migration->cleanFileInfoTable();
 $migration->dropFormerMediaTable($mediaTable);
 $migration->close();
