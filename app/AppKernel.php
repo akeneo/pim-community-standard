35db8194ceb67ba6ceb7aff40eb27f6f4d1dd7c5
Anael CHARDAN <anael.chardan@gmail.com>
2017-08-07 16:09:20 -0400

    MIG-23: Update standard to next version

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -55,14 +55,10 @@ class AppKernel extends Kernel
      */
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+        $loader->load($this->getRootDir() . '/config/config_' . $this->getEnvironment() . '.yml');
 
-        if (is_file($file = __DIR__.'/config/config_'.$this->getEnvironment().'_local.yml')) {
+        if (is_file($file = $this->getRootDir() . '/config/config_' . $this->getEnvironment() . '_local.yml')) {
             $loader->load($file);
-        }
-
-        if (isset($this->bundleMap['DoctrineMongoDBBundle'])) {
-            $loader->load(__DIR__ .'/config/config_mongodb.yml');
         }
     }
 
@@ -130,8 +126,6 @@ class AppKernel extends Kernel
         return [
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle(),
-            // Uncomment the following line to use MongoDB implementation
-            // new Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
@@ -176,5 +170,35 @@ class AppKernel extends Kernel
             new Oro\Bundle\TranslationBundle\OroTranslationBundle(),
             new Oro\Bundle\UserBundle\OroUserBundle(),
         ];
+    }
+
+    /**
+     * @return string
+     */
+    public function getRootDir(): string
+    {
+        return __DIR__;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCacheDir(): string
+    {
+        return dirname(__DIR__)
+            . DIRECTORY_SEPARATOR
+            . 'var'
+            . DIRECTORY_SEPARATOR
+            . 'cache'
+            . DIRECTORY_SEPARATOR
+            . $this->getEnvironment();
+    }
+
+    /**
+     * @return string
+     */
+    public function getLogDir(): string
+    {
+        return dirname(__DIR__) . DIRECTORY_SEPARATOR . 'var' . DIRECTORY_SEPARATOR . 'logs';
     }
 }
