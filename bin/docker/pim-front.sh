2571ec73817585783b43be95fdf23db77f7f2b2f
tamarasaurus <tamara@robichet.com>
2017-09-14 11:24:14 +0200

    TIP-803: Update docker with yarn

--- bin/docker/pim-front.sh
+++ bin/docker/pim-front.sh
@@ -4,4 +4,4 @@ docker-compose exec fpm bin/console --env=prod cache:clear --no-warmup
 
 docker-compose exec fpm bin/console --env=prod pim:installer:assets --symlink --clean
 
-docker-compose run --rm node npm run webpack
+docker-compose run --rm node yarn run webpack
