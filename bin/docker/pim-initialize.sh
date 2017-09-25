b359c6db8d72127d297523f713d2db0d4ec6bcf5
Damien Carcel <damien.carcel@akeneo.com>
2017-09-25 17:35:49 +0200

    Fix "pim-initialize" docker script No need to install behat env

--- bin/docker/pim-initialize.sh
+++ bin/docker/pim-initialize.sh
@@ -3,4 +3,3 @@
 docker-compose exec akeneo app/console --env=prod cache:clear --no-warmup
 
 docker-compose exec akeneo app/console --env=prod pim:install --force --symlink --clean
-docker-compose exec akeneo-behat app/console --env=behat pim:installer:db
