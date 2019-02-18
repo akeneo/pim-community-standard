12202aefb7fe9088d4dea73128746001b6c8fa8f
Tamara Robichet <tamara.robichet@akeneo.com>
2019-02-18 16:11:39 +0100

    TIP-820: Replace assetic bundle with lessjs

--- bin/docker/pim-front.sh
+++ bin/docker/pim-front.sh
@@ -4,4 +4,5 @@ docker-compose exec fpm rm -rf var/cache/*
 
 docker-compose exec fpm bin/console --env=prod pim:installer:assets --symlink --clean
 
+docker-compose run --rm node yarn run less
 docker-compose run --rm node yarn run webpack-dev
--- bin/docker/pim-initialize.sh
+++ bin/docker/pim-initialize.sh
@@ -4,4 +4,5 @@ docker-compose exec fpm rm -rf var/cache/*
 
 docker-compose exec fpm bin/console --env=prod pim:install --force --symlink --clean
 
+docker-compose run --rm node yarn run less
 docker-compose run --rm node yarn run webpack-dev
