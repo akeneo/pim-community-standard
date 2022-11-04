b06e3e537abd0e49d44bca3b37f470fcbef94aea
Steven VAIDIE <steven.vaidie@gmail.com>
2022-11-04 12:26:52 +0100

    RAB-1127: Use PHP 8.1 in order to launch composer install on standard edition

--- .circleci/test_migrations_from_6_0.sh
+++ .circleci/test_migrations_from_6_0.sh
@@ -107,7 +107,7 @@ docker run --user www-data --rm \
   --volume $(pwd):/srv/pim --volume ~/.composer:/var/www.composer --volume ~/.ssh:/var/www/.ssh \
   --workdir /srv/pim \
   --env COMPOSER_AUTH \
-  akeneo/pim-php-dev:master \
+  akeneo/pim-php-dev:8.1 \
   composer install --no-interaction
 
 sudo rm -rf ${PROJECT_DIR}/var/cache/*
